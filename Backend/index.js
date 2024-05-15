f347025d9ef516dd36ea757b4bef1fd5729f1a80
Hridayash Pariyar <hridayashpariyar@Hridayashs-MacBook-Pro.local>
2024-05-15 14:53:02 -0400

    BootStrap The Projects frontend and backend

---
+++ Backend/index.js
@@ -0,0 +1,23 @@
+//installing the packages 
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+
+//setting up the app 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get("/", (req,res)=>{
+    res.send("hello world")
+})
+
+app.listen("3000", (req,res)=>{
+    console.log("server is running on port 3000")
+})
+
+
